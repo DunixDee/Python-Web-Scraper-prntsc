0a0871208bb2656de9006882c436b1659299a035
DunixDee <57366831+DunixDee@users.noreply.github.com>
2019-12-30 02:40:15 +0200

    Image Validation
    Link is randomly generated
    Image URL existance check
    
    -It takes 1-3 seconds to execute, needs to be sped up somehow

--- scr.py
+++ scr.py
@@ -5,22 +5,26 @@ import string
 import os
 import shutil
 
-html = '<html><head><title>The Dormouses story</title></head><body><p class="title"><b>The Dormouses story</b></p><p class="story">Once upon a time there were three little sisters; and their names were<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;nd they lived at the bottom of a well.</p><p class="story">...</p>'
-#For testing purposes, piece of html
+def imgValidate():
+    for i in range(6):
+        url+=random.choice(string.ascii_lowercase)
+    content = requests.get(url, headers = headers).text
+    soup = BeautifulSoup(content, 'html.parser')
+    imgUrl = soup.find_all("img")[0].get('src')
+    if imgUrl in WrongUrl:
+        return imgValidate(WrongUrl);
+    else:
+        return imgUrl, url;
 
-url = 'https://prnt.sc/qabada'
+url = 'https://prnt.sc/'
 WrongUrl = [
-	'st.prntscr.com/2019/11/26/0154/img/0_173a7b_211be8ff.png'
-	]
+    '//st.prntscr.com/2019/11/26/0154/img/0_173a7b_211be8ff.png'
+]
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
 }
-content = requests.get(url, headers = headers).text
-soup = BeautifulSoup(content, 'html.parser')
-
-imgUrl = soup.find_all("img")[0].get('src')
+imgUrl, url = imgValidate()
 imgExt = imgUrl.split('.')[-1]
-
 
 response = requests.get(imgUrl, stream=True)
 with open(os.getcwd()+'\\temp\\'+url[-6:]+'.'+imgExt, 'wb') as out_file:
